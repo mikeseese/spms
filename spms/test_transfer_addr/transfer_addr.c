dee674e7fbfe4483622a103b2d9d4ca29d2c1e86
Mike Seese <seesemichaelj@gmail.com>
2016-12-08 23:56:52 -0500

    Enable logging

--- spms/test_transfer_addr/transfer_addr.c
+++ spms/test_transfer_addr/transfer_addr.c
@@ -3,12 +3,12 @@
 
 int main() {
     SPMS_Initialize(5,10);
-    /*printf("Test Transfer App - Head 1 Addr: %x\n", SPM1_Head);
+    printf("Test Transfer App - Head 1 Addr: %x\n", SPM1_Head);
     printf("Test Transfer App - Tail 1 Addr: %x\n", SPM1_Tail);
     printf("Test Transfer App - Head 2 Addr: %x\n", SPM2_Head);
-    printf("Test Transfer App - Tail 2 Addr: %x\n", SPM2_Tail);*/
+    printf("Test Transfer App - Tail 2 Addr: %x\n", SPM2_Tail);
     *(char*)SPM1_Head = 5;
-    //printf("Test Transfer App - Value at 0 offset of SPM1 head: %d\n", *((char*)SPM1_Head));
+    printf("Test Transfer App - Value at 0 offset of SPM1 head: %d\n", *((char*)SPM1_Head));
     SPMS_Write_Memory_Addresses_To_Sim();
 
     return 0;
