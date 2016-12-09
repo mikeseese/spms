0afad3327954cd428b8e5c2a9e74a4cc033766a8
Mike Seese <seesemichaelj@gmail.com>
2016-12-09 01:45:37 -0500

    split latencies test into two separate tests

---
+++ spms/test_latencies/test_latency_ram.c
@@ -0,0 +1,16 @@
+
+#include <SPMS.h>
+
+int main() {
+    int i;
+    char c;
+    char* mem = malloc(10000);
+    SPMS_Initialize(10000,1);
+    SPMS_Write_Memory_Addresses_To_Sim();
+
+    for(i = 0; i < 10000; i++) {
+        c = ((char*)mem)[i];
+    }
+
+    return 0;
+}
