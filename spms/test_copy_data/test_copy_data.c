c56fcc9d1cd4b17f7dd814ea17ec2cc2a1d11814
Mike Seese <seesemichaelj@gmail.com>
2016-12-09 02:30:52 -0500

    Reverse the src/dst pointers for the copy

--- spms/test_copy_data/test_copy_data.c
+++ spms/test_copy_data/test_copy_data.c
@@ -9,7 +9,7 @@ int main() {
     printf("Test Copy App - Value at 0 offset of SPM1 head: %d\n", *((char*)SPM1_Head));
     printf("Test Copy App - Value at 0 offset of SPM2 head: %d\n", *((char*)SPM2_Head));
     printf("Test Copy App - Copying SPM2_Head[0-999] to SPM1_Head[0-999]\n");
-    SPMS_Copy_Data(SPM1_Head, SPM2_Head, 1000);
+    SPMS_Copy_Data(SPM2_Head, SPM1_Head, 1000);
     printf("Test Copy App - Value at 0 offset of SPM1 head: %d\n", *((char*)SPM2_Head));
 
     return 0;
