2760966e2f28beae301c2c6a20b70e1997fa6bc5
Mike Seese <seesemichaelj@gmail.com>
2016-12-09 00:22:16 -0500

    add appropriate copy logging

--- spms/test_copy_data/test_copy_data.c
+++ spms/test_copy_data/test_copy_data.c
@@ -3,13 +3,14 @@
 
 int main() {
     SPMS_Initialize(5,10);
-    printf("Test Transfer App - Head 1 Addr: %x\n", SPM1_Head);
-    printf("Test Transfer App - Tail 1 Addr: %x\n", SPM1_Tail);
-    printf("Test Transfer App - Head 2 Addr: %x\n", SPM2_Head);
-    printf("Test Transfer App - Tail 2 Addr: %x\n", SPM2_Tail);
     *(char*)SPM1_Head = 5;
-    printf("Test Transfer App - Value at 0 offset of SPM1 head: %d\n", *((char*)SPM1_Head));
+    *(char*)SPM2_Head = 0;
     SPMS_Write_Memory_Addresses_To_Sim();
+    printf("Test Copy App - Value at 0 offset of SPM1 head: %d\n", *((char*)SPM1_Head));
+    printf("Test Copy App - Value at 0 offset of SPM2 head: %d\n", *((char*)SPM2_Head));
+    printf("Test Copy App - Copying SPM1_Head[0] to SPM2_Head[0]\n");
+    SPMS_Copy_Data(SPM1_Head, SPM2_Head, 1);
+    printf("Test Copy App - Value at 0 offset of SPM2 head: %d\n", *((char*)SPM2_Head));
 
     return 0;
 }
